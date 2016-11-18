695925e86928dc1a34f56eddd1844039a1512b6b
Shawn <shawnm0705@gmail.com>
2016-11-18 11:57:58 +1100

    percentage chart status

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 $(window).scroll(function() {
+	var status = document.getElementById('chart_status');
    	var hT = $('#skills').offset().top,
        	hH = $('#skills').outerHeight(),
        	wH = $(window).height(),
@@ -9,10 +10,11 @@ $(window).scroll(function() {
        	console.log(wS);
        	console.log('----');*/
     //console.log((hT-wH) , wS);
-   	if (wS > (hT+hH-wH) && wS < (hT+hH-wH+5)){
+   	if (wS > (hT+hH-wH) && !status.innerHTML){
       	percentage('can1', 100, 20, '#35AFBA', '#DDD', '30px Arial', '20px Arial', 25, 25, 20, 80, 'PHP');
       	percentage('can2', 100, 20, '#FF6060', '#DDD', '30px Arial', '20px Arial', 25, 50, 20, 70, 'HTML/CSS');
       	percentage('can3', 100, 20, '#3AD079', '#DDD', '30px Arial', '20px Arial', 25, 75, 20, 75, 'Javascript/jQuery');
       	percentage('can4', 100, 20, '#58C0E3', '#DDD', '30px Arial', '20px Arial', 25, 25, 20, 65, 'MySQL');
+      	status.innerHTML = 1;
    	}
 });
